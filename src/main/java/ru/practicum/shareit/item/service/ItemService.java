0f0d22ab24197f0702032351e0043cc3a81ce8ab
adenosyl <rustem.kushtayev@gmail.com>
2026-01-07 18:12:37 +0500

    фикс checkstyle #1

--- src/main/java/ru/practicum/shareit/item/service/ItemService.java
+++ src/main/java/ru/practicum/shareit/item/service/ItemService.java
@@ -5,9 +5,14 @@ import ru.practicum.shareit.item.dto.ItemDto;
 import java.util.List;
 
 public interface ItemService {
+
     ItemDto create(Long userId, ItemDto itemDto);
+
     ItemDto update(Long userId, Long itemId, ItemDto itemDto);
+
     ItemDto getById(Long userId, Long itemId);
+
     List<ItemDto> getByOwner(Long userId);
+
     List<ItemDto> search(String text);
 }
